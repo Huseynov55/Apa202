469fe533d3bfd96777ebd7a23bda0891089afdb7
Huseynov55 <anarchh-apa202@code.edu.az>
2026-03-14 22:28:51 +0400

    Project2

---
+++ 02-HeapStackRefOutArrayResize/02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02_HeapStackRefOutArrayResize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = [ 1, 2, 3, 4 ];
+            int[] arr2 = [ 5, 6, 7, 8 ];
+            CustomArrResize(ref arr1, arr2);
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+        }
+        public static void CustomArrResize(ref int[] array1, int[] array2)
+        {
+            int[] newarr= new int[array1.Length + array2.Length];
+            for (int i = 0; i < newarr.Length; i++)
+            {
+                if (i>=array1.Length)
+                {
+                    newarr[i] = array2[i-array1.Length];
+                }
+                else
+                {
+                    newarr[i] = array1[i];
+                }
+            }
+            array1 = newarr;
+        }
+    }
+}
