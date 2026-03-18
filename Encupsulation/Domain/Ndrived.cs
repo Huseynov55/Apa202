3847dddb5df8c1d0b2bf0eac93af9b8ac30299a3
Huseynov55 <anarchh-apa202@code.edu.az>
2026-03-18 23:29:51 +0400

    Project4

---
+++ Encupsulation/Domain/Ndrived.cs
@@ -0,0 +1,33 @@
+﻿using _04_AccessModifiresEncupsulationNamespace.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    internal class Ndrived
+    {
+        public Ndrived()
+        {
+            Person person = new Person();
+            // public fielde non-drive class(different assembly) cata bilirik.
+            Console.WriteLine(person.name);
+
+            // protected fielde non-drive class(different assembly) cata bilmirik.
+            //Console.WriteLine(person.name1);
+
+            // internal fielde non-drive class(different assembly) cata bilmirik.
+            //Console.WriteLine(person.name2);
+
+            // private fielde non-drive class(different assembly) cata bilmirik.
+            //Console.WriteLine(person.name3);
+
+            // protected internal fielde non-drive class(different assembly) cata bilmirik.
+            //Console.WriteLine(person.name4);
+
+            // private protected fielde non-drive class(different assembly) cata bilmirik.
+            //Console.WriteLine(person.name5);
+
+        }
+    }
+}
---
+++ Encupsulation/Domain/Work.cs
@@ -0,0 +1,32 @@
+﻿using _04_AccessModifiresEncupsulationNamespace.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    internal class Work : Person
+    {
+        public Work()
+        {
+            Person person = new Person();
+            // public fielde drived class(different assembly) cata bilirik.
+            Console.WriteLine(this.name);
+
+            // protected fielde drived class(different assembly) cata bilirik.
+            Console.WriteLine(this.name1);
+
+            // internal fielde drived class(different assembly) cata bilmirik.
+            //Console.WriteLine(this.name2);
+
+            // private fielde drived class(different assembly) cata bilmirik.
+            //Console.WriteLine(this.name3);
+
+            // protected internal fielde drived class(different assembly) cata bilirik.
+            Console.WriteLine(this.name4);
+
+            // private protected fielde drived class(different assembly) cata bilmirik.
+            //Console.WriteLine(this.name5);
+        }
+    }
+}
