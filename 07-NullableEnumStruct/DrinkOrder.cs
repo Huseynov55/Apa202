1ecc7a35e4d09103671c55451e666ba53421aa70
Huseynov55 <anarchh-apa202@code.edu.az>
2026-03-31 01:47:20 +0400

    Project7

---
+++ 07-NullableEnumStruct/DrinkOrder.cs
@@ -0,0 +1,72 @@
+﻿using _07_NullableEnumStruct.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_NullableEnumStruct
+{
+    internal class DrinkOrder
+    {
+        public int OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public DrinkType Drink { get; set; }
+        public DrinkSize Size { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal Price { get; private set; }
+
+
+        public DrinkOrder(int orderNumber, string customerName, DrinkType drink, DrinkSize size)
+        {
+            OrderNumber = orderNumber;
+            CustomerName = customerName;
+            Drink = drink;
+            Size = size;
+            Status = OrderStatus.New;
+            Price = CalculatePrice();
+        }
+
+        public decimal CalculatePrice()
+        {
+            switch (Drink)
+            {
+                case DrinkType.Coffee:
+                    if (Size == DrinkSize.Small) return 3;
+                    if (Size == DrinkSize.Medium) return 4;
+                    return 5;
+
+                case DrinkType.Tea:
+                    if (Size == DrinkSize.Small) return 2;
+                    if (Size == DrinkSize.Medium) return 3;
+                    return 4;
+
+                case DrinkType.Juice:
+                    if (Size == DrinkSize.Small) return 4;
+                    if (Size == DrinkSize.Medium) return 5;
+                    return 6;
+
+                case DrinkType.Water:
+                    if (Size == DrinkSize.Small) return 1;
+                    if (Size == DrinkSize.Medium) return 1.5m;
+                    return 2;
+
+                default:
+                    return 0;
+            }
+        }
+
+        public void UpdateStatus(OrderStatus newStatus)
+        {
+            Status = newStatus;
+            Console.WriteLine($"Sifariş #{OrderNumber} statusu: {newStatus}");
+        }
+
+        public void DisplayOrder()
+        {
+            Console.WriteLine($"Sifariş Nomresi: {OrderNumber}");
+            Console.WriteLine($"Musteri: {CustomerName}");
+            Console.WriteLine($"İcki: {Drink} ({Size})");
+            Console.WriteLine($"Qiymet: {Price} AZN");
+            Console.WriteLine($"Status: {Status}");
+        }
+    }
+}
---
+++ 07-NullableEnumStruct/Program.cs
@@ -0,0 +1,55 @@
+﻿using _07_NullableEnumStruct.Enums;
+
+namespace _07_NullableEnumStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinkOrder s1 = new DrinkOrder(101, "Ali", DrinkType.Coffee, DrinkSize.Medium);
+            s1.DisplayOrder();
+            s1.UpdateStatus(OrderStatus.Preparing);
+            s1.UpdateStatus(OrderStatus.Ready);
+            s1.UpdateStatus(OrderStatus.Delivered);
+
+
+            DrinkOrder s2 = new DrinkOrder(102, "Leyla", DrinkType.Tea, DrinkSize.Large);
+            s2.DisplayOrder();
+            s2.UpdateStatus(OrderStatus.Ready);
+            s2.DisplayOrder();
+
+
+            DrinkOrder s3 = new DrinkOrder(103, "Vuqar", DrinkType.Juice, DrinkSize.Small);
+            s3.DisplayOrder();
+
+
+
+            Console.WriteLine("İcki Novleri:");
+            foreach (DrinkType d in Enum.GetValues(typeof(DrinkType)))
+            {
+                Console.WriteLine("- " + d);
+            }
+
+            Console.WriteLine(DrinkType.Coffee.ToString());
+
+
+            string strTea = "Tea";
+            DrinkType parsedTea = (DrinkType)Enum.Parse(typeof(DrinkType), strTea);
+            Console.WriteLine(parsedTea);
+
+            Console.WriteLine("\n--- Statistika ---");
+            decimal totalAmount = s1.Price + s2.Price + s3.Price;
+
+
+            Console.WriteLine($"Umumi sifaris: 3");
+            Console.WriteLine($"1-ci sifaris: {s1.Price} AZN");
+            Console.WriteLine($"2-ci sifaris: {s2.Price} AZN");
+            Console.WriteLine($"3-cu sifaris: {s3.Price} AZN");
+            Console.WriteLine($"Umumi mebleg: {totalAmount} AZN");
+
+
+
+
+        }
+    }
+}
